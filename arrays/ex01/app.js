9e1fcf0e37f6992608220a20fccda22180f1aa08
SAMUEL ALAN GENTIL LOPES <samuel.aglopes@senacsp.edu.br>
2025-09-19 22:07:02 -0300

    Comentario

---
+++ arrays/ex01/app.js
@@ -0,0 +1,3 @@
+let frutas = ["A    bacate", "Banana", "Caju", "Damasco", "Embaúba", "Figo", "Goiaba", "Hibisco", "Imbu", "Jabuticaba", "kiwi", "laranja"]
+
+console.log(frutas)
---
+++ arrays/ex02/app.js
@@ -0,0 +1,9 @@
+let tarefa = ["Estudar", "lavar a louça", "Caminhar"]
+
+for (let i = 0; i < tarefa.length; i ++) {
+    console.log(tarefa.length);
+    console.log(tarefa[i])
+
+    console.log(`Tarefa na posição ${i}: ${tarefa[i]}`)
+
+}
